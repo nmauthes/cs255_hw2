54c77c6d43969457e8870742a98fc46d9f1ffa33
Nicolas Mauthes <nmauthes@netscape.net>
2018-02-24 11:45:33 -0800

    start threading code

--- ThreadInner.py
+++ ThreadInner.py
@@ -4,11 +4,41 @@ Computes the inner product of two vectors using threading.
 :authors Kunal, Nick, Shashank
 '''
 
+
 import argparse
+import os
+import time
 import threading
 
 
 # Code goes here
+
+
+def inner_product(x, y):
+    return sum([x_i * y_i for x_i, y_i in zip(x, y)])
+
+
+def inner_product_parallel(x, y):
+    pass
+
+
+def parse_vector_file(filename):
+    if os.path.exists(filename):
+        pass
+    else:
+        raise Exception('Vector file not found')
+
+    x = []
+    y = []
+
+    with open(filename) as f:
+        for line in f:
+            x_i, y_i = line.split(',')
+
+            x.append(int(x_i))
+            y.append(int(y_i))
+
+    return x, y
 
 
 parser = argparse.ArgumentParser(
@@ -24,4 +54,7 @@ parser.add_argument(
 
 
 if __name__ == '__main__':
-    args = parser.parse_args()  # Main method goes here
+    args = parser.parse_args()
+
+    x, y = parse_vector_file(args.filename_with_vector_data)
+    print(inner_product(x, y))
